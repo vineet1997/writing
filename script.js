acd9a6d43b8fd1504119aa7beed4da0a616a3203
Vineet <vineetagarwal98@gmail.com>
2024-11-19 00:53:30 +0530

    Using a fixed set of words

--- script.js
+++ script.js
@@ -12,11 +12,32 @@
 //         .catch((error) => "Error fetching words!");
 // }
 
+// function generateRandomWords() {
+//     return fetch("https://random-word-api.herokuapp.com/word?number=3")
+//         .then((response) => response.json())
+//         .then((data) => data.join(", "))
+//         .catch((error) => "Error fetching words: " + error.message);
+// }
+
+let commonWords = [];
+
+fetch("common-words.json")
+    .then((response) => response.json())
+    .then((data) => {
+        commonWords = data;
+    })
+    .catch((error) => console.error("Error loading common words:", error));
+
 function generateRandomWords() {
-    return fetch("https://random-word-api.herokuapp.com/word?number=3")
-        .then((response) => response.json())
-        .then((data) => data.join(", "))
-        .catch((error) => "Error fetching words: " + error.message);
+    if (commonWords.length === 0) {
+        return "Common words list not loaded yet!";
+    }
+    const randomWords = [];
+    for (let i = 0; i < 3; i++) {
+        const randomIndex = Math.floor(Math.random() * commonWords.length);
+        randomWords.push(commonWords[randomIndex]);
+    }
+    return randomWords.join(", ");
 }
 
 document.getElementById("generate-btn").addEventListener("click", async () => {
